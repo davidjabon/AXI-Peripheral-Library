6c06937e3b7ab5571c1f06cb6edaa86d6bc62dd4
davidjabon <djabon@depaul.edu>
2015-09-21 23:38:02 -0500

    Update Eight_Digit_Seven_Segment_Display.c

--- Eight_Digit_Seven_Segment_Display_2.0/drivers/Eight_Digit_Seven_Segment_Display_v1_0/src/Eight_Digit_Seven_Segment_Display.c
+++ Eight_Digit_Seven_Segment_Display_2.0/drivers/Eight_Digit_Seven_Segment_Display_v1_0/src/Eight_Digit_Seven_Segment_Display.c
@@ -4,29 +4,30 @@
 #include "Eight_Digit_Seven_Segment_Display.h"
 
 /************************** Function Definitions ***************************/
-void Write_BCD_to_SSD(int baseaddress, int n)
+void Write_BCD_to_SSD(u32 BaseAddress, u32 n)
 {
-	Xil_Out32(baseaddress, n);
+	Xil_Out32(BaseAddress, n);
 }
 
 
-void Write_int_to_SSD(int baseaddress, int n)
+void Write_int_to_SSD(u32 BaseAddress, u32 n)
 {
-	Xil_Out32(baseaddress, int2bcd(n));
+	Xil_Out32(BaseAddress, int2bcd(n));
 }
 
 
-void Set_decimal_points(int baseaddress, int n)
+void Set_decimal_points(u32 BaseAddress, u32 n)
 {
-	Xil_Out32(baseaddress + EIGHT_DIGIT_SEVEN_SEGMENT_DISPLAY_S00_AXI_SLV_REG1_OFFSET, n);
+	Xil_Out32(BaseAddress + EIGHT_DIGIT_SEVEN_SEGMENT_DISPLAY_S00_AXI_SLV_REG1_OFFSET, n);
 }
 
-int int2bcd(int n)
+
+u32 int2bcd(u32 n)
 {
-    int out=0;
-    int n_reduced=n;
-    int digit=0;
-    int i=0;
+    u32 out=0;
+    u32 n_reduced=n;
+    u32 digit=0;
+    u32 i=0;
 
     while( (n_reduced > 0) && (i<8))
     {
