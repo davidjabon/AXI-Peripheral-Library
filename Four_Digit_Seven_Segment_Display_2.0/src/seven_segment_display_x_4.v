b3dc04adc391b611f0eb07080a3c69f7284a83a4
davidjabon <djabon@depaul.edu>
2015-09-27 09:50:23 -0500

    Update seven_segment_display_x_4.v

--- Four_Digit_Seven_Segment_Display_2.0/src/seven_segment_display_x_4.v
+++ Four_Digit_Seven_Segment_Display_2.0/src/seven_segment_display_x_4.v
@@ -15,7 +15,9 @@
 // 
 // Revision:
 // Revision 0.01 - File Created
-// Additional Comments:
+// Additional Comments: Note that the segments are a_to_g with a in position 0, b in position 1, etc.
+//                      The reverse assignments are commented out to the right.
+//			            To turn off decimal point set the corresponding bit low.
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -35,30 +37,30 @@ reg [19:0] clkdiv;
          
 assign counter = clkdiv[19:18];   //count every 2.6 ms  (with 100 MHz clock)
     
-    //4 to 1 MUX
-    always @(posedge clk)
-        case(counter)
-            0: {digit, decimal_point, anode} = {bcd_in[3:0], ~decimal_points[0],  4'b1110};
-            1: {digit, decimal_point, anode} = {bcd_in[7:4], ~decimal_points[1],  4'b1101};
-            2: {digit, decimal_point, anode} = {bcd_in[11:8], ~decimal_points[2], 4'b1011};
-            3: {digit, decimal_point, anode} = {bcd_in[15:12], ~decimal_points[3],4'b0111};
-         endcase
+//4 to 1 MUX
+always @(posedge clk)
+    case(counter)
+        0: {digit, decimal_point, anode} = {bcd_in[3:0], ~decimal_points[0],  4'b1110};
+        1: {digit, decimal_point, anode} = {bcd_in[7:4], ~decimal_points[1],  4'b1101};
+        2: {digit, decimal_point, anode} = {bcd_in[11:8], ~decimal_points[2], 4'b1011};
+        3: {digit, decimal_point, anode} = {bcd_in[15:12], ~decimal_points[3],4'b0111};
+     endcase
     
-    //7-segment decoder 
-    always @(posedge clk)
-         case(digit)
-            0: a_to_g = 8'b1000000;  //8'b0000001;
-            1: a_to_g = 8'b1111001;  //8'b1001111;
-            2: a_to_g = 8'b0100100;  //8'b0010010;
-            3: a_to_g = 8'b0110000;  //8'b0000110;
-            4: a_to_g = 8'b0011001;  //8'b1001100;
-            5: a_to_g = 8'b0010010;  //8'b0100100;
-            6: a_to_g = 8'b0000010;  //8'b0100000;
-            7: a_to_g = 8'b1111000;  //8'b0001111;
-            8: a_to_g = 8'b0000000;  //8'b0000000;
-            9: a_to_g = 8'b0010000;  //8'b0000100;
-            default: a_to_g = 8'b11111111;  //default to blank
-        endcase
+//7-segment decoder 
+always @(posedge clk)
+     case(digit)
+        0: a_to_g = 8'b1000000;  // 8'b0000001;
+        1: a_to_g = 8'b1111001;  // 8'b1001111;
+        2: a_to_g = 8'b0100100;  // 8'b0010010;
+        3: a_to_g = 8'b0110000;  // 8'b0000110;
+        4: a_to_g = 8'b0011001;  // 8'b1001100;
+        5: a_to_g = 8'b0010010;  // 8'b0100100;
+        6: a_to_g = 8'b0000010;  // 8'b0100000;
+        7: a_to_g = 8'b1111000;  // 8'b0001111;
+        8: a_to_g = 8'b0000000;  // 8'b0000000;
+        9: a_to_g = 8'b0010000;  // 8'b0000100;
+        default: a_to_g = 8'b11111111;  //default to blank
+    endcase
           
          
 //clock divider 
